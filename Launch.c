97966512a07357768ba808392bb38a96dbc3ba62
sirLobardo <61591862+sirLobardo@users.noreply.github.com>
2020-05-28 10:46:39 -0300

    Add files via upload

---
+++ Launch.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "menu.h"
+
+int main()
+{
+    menu();
+
+    system("pause");
+    return 0;
+}
---
+++ menu.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void menu()
+{
+
+    printf("Bem-vindo(a) as Batalhas da Laponia \n");
+    printf("Aperte 1 para comecar um novo jogo \n");
+    printf("Aperte 2 para ver como jogar \n");
+    printf("Aperte 3 para sair \n");
+}
