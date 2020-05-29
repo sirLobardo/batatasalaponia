f238f942b155bcc9f64f38739f5d93bc3dee79e3
sirLobardo <sirlobardo@protonmail.com>
2020-05-29 16:16:22 -0300

    Essa biblioteca agora será chamada de victor.h

--- menu.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void menu()
-{
-
-    printf("Bem-vindo(a) as Batalhas da Laponia \n");
-    printf("Aperte 1 para comecar um novo jogo \n");
-    printf("Aperte 2 para ver como jogar \n");
-    printf("Aperte 3 para sair \n");
-}
