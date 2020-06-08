4c25041e38db4d2d3a66287871890ae56a595e8c
flybaruk <66071456+flybaruk@users.noreply.github.com>
2020-06-08 17:54:02 -0300

    Update guilherme.h

--- guilherme.h
+++ guilherme.h
@@ -12,24 +12,84 @@
 	  }		   
    }
  void marcar1(int lin, int col) {
- 	if(lin < 2 || col < 2 || lin > 11 || col > 11){
+ 	if(lin < 2 || col < 2 || lin > 11 || col > 11)
+	 {
 		 printf("Nao eh possivel marcar os soldados nos morros\n\n");		 	 
 	 }
-	if(lin == 6 || lin == 7)	{
+	if(lin == 6 || lin == 7)
+		{
 		printf("Nao eh possivel marcar os soldados no rio\n\n");
 	} 
 	 else	 {
 		 mapa [lin][col] = '1';
 	 }	  
  }
+ 
+ void marcar2(int lin, int col) {
+ 	if(lin < 2 || col < 2 || lin > 11 || col > 11)
+	 {
+		 printf("Nao eh possivel marcar os soldados nos morros\n\n");		 	 
+	 }
+	if(lin == 6 || lin == 7)
+		{
+		printf("Nao eh possivel marcar os soldados no rio\n\n");
+	} 
+	 else	 {
+		 mapa [lin][col] = '2';
+	 }	  
+ }
+ 
+ void heroinorte(int lin, int col) {
+ 	if(lin < 2 || col < 2 || lin > 11 || col > 11)
+	 {
+		 printf("Nao eh possivel marcar os soldados nos morros\n\n");		 	 
+	 }
+	if(lin == 6 || lin == 7)
+		{
+		printf("Nao eh possivel marcar os soldados no rio\n\n");
+	} 
+	 else	 {
+		 mapa [lin][col] = '#';
+	 }	  
+ }
+ 
+ void heroisul(int lin, int col) {
+ 	if(lin < 2 || col < 2 || lin > 11 || col > 11)
+	 {
+		 printf("Nao eh possivel marcar os soldados nos morros\n\n");		 	 
+	 }
+	if(lin == 6 || lin == 7)
+		{
+		printf("Nao eh possivel marcar os soldados no rio\n\n");
+	} 
+	 else	 {
+		 mapa [lin][col] = '@';
+	 }	  
+ }
       void marcarsoldados()   {
 	 int i, j, cont;
 	 imprimirmapa();
-	   for(cont = 1; cont <= 8; cont++)	   {
-	 printf("Digite a linha e a coluna que deseja posicionar o soldado: ");
+	   for(cont = 1; cont <= 7; cont++)	   {
+	 printf("Digite a linha e a coluna que deseja posicionar o soldado 1 : ");
 	 scanf("%d %d", &i, &j);
 	     system("cls");	   
 		marcar1(i, j); 
     imprimirmapa();
+    printf("Digite a linha e a coluna que deseja posicionar o soldado 2 : ");
+     scanf("%d %d", &i, &j);
+	     system("cls");
+        marcar2(i, j);
+    imprimirmapa();
+    } 
+      printf("Digite a localização do soldado especial do norte: ");
+        scanf("%d %d", &i, &j);
+           system("cls");
+          heroinorte(i,j);
+	imprimirmapa();	   
+	   printf("Digite a localização do soldado especial do sul: ");
+        scanf("%d %d", &i, &j);
+           system("cls");
+          heroisul(i,j);
+	imprimirmapa();
     } 
       }
