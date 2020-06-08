a4b0b22e37faa1aed6d5889dad49fd50579f0a7e
flybaruk <66071456+flybaruk@users.noreply.github.com>
2020-06-08 12:04:50 -0300

    Update guilherme.h

--- guilherme.h
+++ guilherme.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
  
   void imprimirmapa(){
+
 	   int lin, col;
 	 	 for(lin = 0; lin < 13; lin++)	  {
 		  for(col = 0; col < 13; col++)		  {
@@ -10,20 +11,23 @@
 		  printf("\n");
 	  }		   
    }
-
  void marcar1(int lin, int col) {
- 	if(lin < 2 || col < 2 || lin > 11 || col > 11 || lin == 6 || lin == 7){
-		 printf("coordenadas invalidas\n\n");		 	 
-	 }else	 {
+ 	if(lin < 2 || col < 2 || lin > 11 || col > 11){
+		 printf("Nao eh possivel marcar os soldados nos morros\n\n");		 	 
+	 }
+	if(lin == 6 || lin == 7)	{
+		printf("Nao eh possivel marcar os soldados no rio\n\n");
+	} 
+	 else	 {
 		 mapa [lin][col] = '1';
 	 }	  
  }
- int marcarsoldados() {
-	 int i, j;
+	 int i, j, cont;
 	 imprimirmapa();
-	 printf("Digite a linha e a coluna que deseja posicionar o soldado:");
+	   for(cont = 1; cont <= 8; cont++)	   {
+	 printf("Digite a linha e a coluna que deseja posicionar o soldado: ");
 	 scanf("%d %d", &i, &j);
-	 marcar1(i, j);
-	 system("cls");
-	 imprimirmapa();
-	 
+	     system("cls");	   
+		marcar1(i, j); 
+    imprimirmapa();
+    } 
