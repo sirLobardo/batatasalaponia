dd35958f96f20a36f61b173b995d04ae2da064b1
Monokumerz <65869623+Monokumerz@users.noreply.github.com>
2020-06-10 14:51:21 -0300

    Update victor.h

--- victor.h
+++ victor.h
@@ -23,22 +23,24 @@ void comojogar(){
 system("cls");
 
 
-printf("INTRODUCAO \n");
+printf("-------INTRODUCAO------- \n");
 printf("\n");
 
 printf("A Lapônia era uma região de 12 km2 situada na costa sul da África do Sul, a qual era cercada por uma grande cordilheira, banhada pelo grande lago “Ichibi Elikhulu”. \n");
+system("pause"); 
 printf("Esta região foi disputada por facções guerreiras do norte e do sul, separadas apenas pelo grande lago. Era impossível transpor o lago, portanto as facções criaram técnicas e armamentos para travar disputas entre si de um lado ao outro do lago. \n");
+system("pause");
 printf("Não há um consenso histórico sobre qual lado saiu vitorioso, já que a região foi inundada pouco tempo depois do fim do conflito, dizimando todos que lá viviam \n");
+system("pause");
 printf("Apenas uma guerreira conseguiu fugir da Lapônia, chamada Iqhawe Elihlakaniphile, que criou esse jogo para manter vivo o histórico de seu povo \n");
-printf("\n");
-printf("aperte enter para continuar \n");
+system("pause");
 }
 
 void regras(){
 
 system("cls");
  
-printf("REGRAS \n");
+printf("-------REGRAS------- \n");
 printf("\n");
 printf("1 - O jogo consiste em um tabuleiro de tamanho 12x12, com as casas da borda representando a cordilheira e separado exatamente no meio por uma faixa de 20 casas, representando o grande lago \n ");
 system("pause");
