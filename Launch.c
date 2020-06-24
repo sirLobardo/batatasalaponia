72a65e278cd6b5ab981019ada85f5205fd183e8c
CHESSSUS <65869657+CHESSSUS@users.noreply.github.com>
2020-06-24 16:53:55 -0300

    Update Launch.c

--- Launch.c
+++ Launch.c
@@ -1,12 +1,17 @@
+  
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
 #include "varglobal.h"
 #include "guilherme.h"
+#include "heitor.h"
+#include "jogete.h"
 #include "eduardo.h"
 #include "victor.h"
 
+
 int main()
+
 {
     setlocale(LC_ALL, "Portuguese");
     funcao();
